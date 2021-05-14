1850871b0f48aaf2f6f13abc2d88ddc4b84c1403
Shreddan <40694103+Shreddan@users.noreply.github.com>
2021-05-14 13:52:56 +0100

    declarations & definitions

--- EpicTextBasedGame/Weapon.cpp
+++ EpicTextBasedGame/Weapon.cpp
@@ -11,3 +11,28 @@ Weapon::~Weapon()
 Item* Weapon::getItem()
 {
 }
+
+int Weapon::getDamage()
+{
+	return this->Damage;
+}
+
+int Weapon::getType()
+{
+	return this->type;
+}
+
+int Weapon::getEnch1()
+{
+	return this->enchant1;
+}
+
+int Weapon::getEnch2()
+{
+	return this->enchant2;
+}
+
+int Weapon::getEnch3()
+{
+	return this->enchant3;
+}
